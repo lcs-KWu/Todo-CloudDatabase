d1ff484ef7d3b1b97be6576aeecad7acb4d12d60
Kevin Wu <KWu@lcs.on.ca>
2025-04-06 12:34:07 -0400

    Added a helper file to identify our app to the Supabase servers.

---
+++ TodoList/Helpers/SupaBase.swift
@@ -0,0 +1,15 @@
+//
+//  SupaBase.swift
+//  TodoList
+//
+//  Created by Yixuan Wu on 2025-04-06.
+//
+
+import Foundation
+import Supabase
+ 
+let supabase = SupabaseClient(
+  supabaseURL: URL(string: "https://xcketlirpwazojncyose.supabase.co")!,
+  supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhja2V0bGlycHdhem9qbmN5b3NlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM3NzkzNDYsImV4cCI6MjA1OTM1NTM0Nn0.o-Tv-K6DKB97v9To6AaFXn0e8UvwXA_m-K314WtdJlw"
+)
+ 
